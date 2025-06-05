25e203c871ebb45ee957978aed142f27e8a40dcc
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2025-06-05 14:31:52 -0400

    add new elements

--- app.py
+++ app.py
@@ -14,31 +14,32 @@ with open('misc_data/states.txt', 'r') as file:
 # Set up dictionary to store data 
 data = {}
 
-with st.form('addition'):
-    st.title("Patient's Demographic")
+with st.form('BasicInfo'):
+    ####################################################
+    st.header("Appointment Summary")
+    audio_summary = st.audio_input("Summarize the appointment details")
+
+    ####################################################
+    st.header("Patient's data")
     # Dict to store data
-    demographic = {}
-    demographic['firstname'] = st.text_input('Patient First Name')
-    demographic['lastname'] = st.text_input('Patient Last Name')
-    demographic['pronoun'] = st.selectbox(
+    data['{{Patient First Name}}'] = st.text_input('Patient First Name')
+    data['{{Patient Last Name}}'] = st.text_input('Patient Last Name')
+    preferred = st.selectbox(
         "Patient's Preferred Pronoun",
         ("They/them", "He/him", "She/her"),
     )
-    age1,age2 = st.columns(2)
-    with age1:
-        demographic['age_amount'] = st.number_input("Patient's Age", 0, 100)
-    with age2:
-        demographic['age_unit'] = st.selectbox(
-            "Year/month?",
-            ("Year", "Month"),
-        )
-
-    demographic['caregiver'] = st.selectbox(
-        "Patient's Caregiver",
-        ("Mother", "Father", "Parent", "Grandparent", "Legal Custodian", "Foster Parent"),
+    data["{{Patient's Age}}"] = st.number_input("Patient's Age", 0, 100)
+    data['age_unit'] = st.selectbox(
+        "Year/month?",
+        ("Year", "Month"),
     )
 
-    demographic['primary_concern'] = st.multiselect(
+    data['{{Caregiver type}}'] = st.selectbox(
+        "Patient's Caregiver",
+        ("mother", "father", "parent", "grandparent", "legal custodian", "Foster Parent"),
+    )
+
+    data['{{Caregiver\'s Primary Concerns}}'] = st.multiselect(
         "Caregiver\'s Primary Concerns",
         (
             "Speech delays impacting social opportunities.",
@@ -50,51 +51,98 @@ with st.form('addition'):
         ),
         accept_new_options=True
     )
+    data['{{Caregiver\'s Primary Concerns}}'] = "\n".join(['{{Caregiver\'s Primary Concerns}}'])
 
-    demographic['state'] = st.selectbox(
+    data['{{Residence City/State}}'] = st.selectbox(
         "Residence City/State", states
     )
 
-    demographic['narrative'] = st.text_area('Narrative to finish \"Patient lives with...\"')
-    data['Demographic'] = demographic
+    data['{{Narrative}}'] = st.text_area('Narrative to finish \"Patient lives with...\"')
+
+    ##########################################################
+    st.header("BRH Evaluation Details")
+
+    data['{{Evaluation Date}}'] = st.date_input("Evaluation Date")
+
+    data['{{Module used}}'] = st.radio("Module used", ["Module 1", "Module 2"])
+    if data['{{Module used}}'] == "Module 1":
+        data['{{Module Description}}'] = "Module 1 is designed for children with single words"
+    else:
+        data['{{Module Description}}'] = "Module 2 is designed for children with phrase speech"
+
+    data['{{Location of the evaluation}}'] = st.radio(
+        "Location of the evaluation",
+        ['home', 'school', 'the office']
+    )
+
+    data['{{Results Shared Date}}'] = st.date_input("Results Shared Date")
     
+    data['{{Date Report Sent to Patient}}'] = st.date_input("Date Report Sent to Patient")
+
+    data["{{Result of the evaluation}}"] = st.multiselect(
+        "Result of the evaluation",
+        (
+            "F84.0 - Autism Spectrum Disorder (per the above referenced evaluation)",
+            "F88.0 - Global Developmental Delay (per behavioral presentation)",
+            "F80.2 - Mixed Receptive-Expressive Language Disorder",
+            "F90.2 - Attention Deficit Hyperactivity Disorder - Combined-Type",
+            "F50.82 Avoidant/Restrictive Food Intake Disorder",
+            "None",
+        ),
+        accept_new_options=True
+    )
+
+    data["{{Results (SCQ) - Lifetime Form}}"] = st.text_input(
+        "Results (SCQ) - Lifetime Form"
+    )
+
+    data["{{SRS-2 Score Caregiver}}"] = st.text_input("SRS-2 Score Caregiver")
+    
+    data["{{Social Communication and Interaction Score Caregiver}}"] = st.text_input("Social Communication and Interaction Score Caregiver")
+    
+    data["{{Restricted Interests and Repetitive Behavior Score Caregiver}}"] = st.text_input("Restricted Interests and Repetitive Behavior Score Caregiver")
+
+    data["{{Caregiver's level of concern}}"] = st.radio(
+        "Caregiver's level of concern",
+        ['no', 'mild', 'moderate', 'severe']
+    )
+
+    data["{{Evaluator's level of concern}}"] = st.radio(
+        "Evaluator's level of concern",
+        ['no', 'mild', 'moderate', 'severe']
+    )
+
+    teacher_eval = st.checkbox("Teacher gave SSR Scores")
+
+    ######################################################
+
     submit = st.form_submit_button('Submit')
+
+
 
 if submit:
     doc = Document('templates/template_mod_12.docx')
-    yaml_string = yaml.dump(data, sort_keys=False)
-    yaml_data = st.code(yaml_string, language=None)
 
     # handle word to replace 
     # pronouns
     with open("misc_data/pronouns.yaml", "r") as file:
         pronoun = yaml.safe_load(file)
-    preferred = data['Demographic']['pronoun']
 
     replace_word = {
-        "{{Patient First Name}}": data['Demographic']['firstname'],
-        "{{Patient Last Name}}": data['Demographic']['lastname'],
         "{{Preferred Pronouns 1}}": pronoun[preferred]['pronoun1'],
         "{{Preferred Pronouns 1 CAP}}": pronoun[preferred]['pronoun1cap'],
         "{{Preferred Pronouns 2}}": pronoun[preferred]['pronoun2'],
         "{{Preferred Pronouns 2 CAP}}": pronoun[preferred]['pronoun2cap'],
-        "{{Caregiver type}}": data['Demographic']['caregiver'].lower(),
-        "{{Residence City/State}}": data['Demographic']['state'],
-        '{{Narrative to finish \"Patient lives with...\"}}': data['Demographic']['narrative'],
     }
 
-    # for p in doc.paragraphs:
-    #     style = p.style
-    #     for word in replace_word:
-    #         print(type(p.text))
-    #         if p.text.find(word) >= 0:
-    #             p.text = p.text.replace(word, replace_word[word])
-    #     p.style = style
+    replace_word.update(data)
+
+    # Display data 
+    yaml_string = yaml.dump(replace_word, sort_keys=False)
+    yaml_data = st.code(yaml_string, language=None)
     
     for word in replace_word:
         docxedit.replace_string(doc, old_string=word, new_string=replace_word[word])
-
-    # doc.save(f'output/Report_{data['Demographic']['firstname']}_{data['Demographic']['lastname']}.docx')
 
     bio = io.BytesIO()
     doc.save(bio)
@@ -103,6 +151,6 @@ if submit:
         st.download_button(
             label="Click here to download",
             data=bio.getvalue(),
-            file_name=f"Report_{data['Demographic']['firstname']}_{data['Demographic']['lastname']}.docx.docx",
+            file_name=f"Report_{data['{{Patient First Name}}']}_{data['{{Patient Last Name}}']}.docx",
             mime="docx"
         )
