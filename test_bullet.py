010575a3f84c7f6374bf4b35498436d9d9fe6513
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2025-06-20 15:13:08 -0400

    change template to have bullet

--- test_bullet.py
+++ test_bullet.py
@@ -1,14 +1,18 @@
 from docxtpl import DocxTemplate
+import yaml
 
-tpl=DocxTemplate('templates/template_mod_12_noBrackets.docx')
 
-context = {
-    'bullets': [
-        "Awkward social initiation and response",
-        "Difficulties with chit-chat",
-        "Difficulty interpreting figurative language",
-    ],
-}
+tpl=DocxTemplate('output\Linh Tran June 20th, 2025.docx')
+
+with open('data.yaml', 'r') as file:
+    context = yaml.safe_load(file)
+# context = {
+#     'bullets': [
+#         "Awkward social initiation and response",
+#         "Difficulties with chit-chat",
+#         "Difficulty interpreting figurative language",
+#     ],
+# }
 
 tpl.render(context)
 tpl.save("output.docx")
