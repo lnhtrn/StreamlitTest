52b320c809159da0209d05b14d02eba6a2a1ef61
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2025-06-21 23:36:59 -0400

    modify text area

--- audio_test.py
+++ audio_test.py
@@ -15,13 +15,20 @@ st.set_page_config(
 def load_whisper_model():
     return whisper.load_model("base")
 
+# Use text_area key to modify it 
+def on_upper_updated():
+    st.session_state.transcript = st.session_state.transcript.upper()
+
+def on_upper_reload(new_text):
+    st.session_state.transcript = new_text
+
 whisper_model = load_whisper_model()
 
 # Record audio
 audio_data = st.audio_input("Speak something to transcribe")
 
 st.markdown("**Transcription:**")
-editable_trans = st.text_area("Verify and edit transcription", "")
+editable_trans = st.text_area("Verify and edit transcription", key="transcript")
 
 if audio_data:
     # Save audio
@@ -31,8 +38,9 @@ if audio_data:
     # Transcribe
     with st.spinner("Transcribing...", show_time=True):
         result = whisper_model.transcribe("temp.wav")
-    editable_trans = result['text']
+    on_upper_reload(result['text'])
     
 if st.button("Show final text"):
     st.markdown("**Finalized text:**")
+    on_upper_updated()
     st.write(editable_trans)
