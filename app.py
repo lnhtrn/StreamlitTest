57cdaecaa1b6d55ab2c480a8610f9bf17cf7cc35
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2025-06-27 00:03:26 -0400

    remove folder

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("webpages/mod12.py", title="Module 1&2"), 
-    st.Page("webpages/mod12_no.py", title="Module 1&2 No Autism"), 
-    st.Page("webpages/audio_test.py", title="Transcription Testing"),
+    st.Page("mod12.py", title="Module 1&2"), 
+    st.Page("mod12_no.py", title="Module 1&2 No Autism"), 
+    st.Page("audio_test.py", title="Transcription Testing"),
 ])
 pg.run()
