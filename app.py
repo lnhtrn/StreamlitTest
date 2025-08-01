9f41b0161666cef5d605ad7aa1ad981f8b9b558f
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2025-08-01 09:00:28 -0400

    edit sidebar

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import streamlit as st
 pg = st.navigation([
     st.Page("home.py", title="Homepage"), 
     st.Page("mod12.py", title="Module 1&2", icon="📘"), 
-    st.Page("mod12_no.py", title="Module 1&2 No Autism"), 
-    st.Page("mod3.py", title="Module 3"), 
-    st.Page("mod3_no_autism.py", title="Module 3 No Autism"), 
-    st.Page("mod4.py", title="Module 4"), 
-    st.Page("gsheet_test.py", title="Testing Recommendation"),
+    st.Page("mod12_no.py", title="Module 1&2 No Autism", icon="📘"), 
+    st.Page("mod3.py", title="Module 3", icon="📗"), 
+    st.Page("mod3_no_autism.py", title="Module 3 No Autism", icon="📗"), 
+    st.Page("mod4.py", title="Module 4", icon="📕"), 
+    st.Page("gsheet_test.py", title="Testing Recommendation", icon="🛠️"),
 ])
 
 
